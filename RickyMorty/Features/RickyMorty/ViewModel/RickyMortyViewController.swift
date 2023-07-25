dd4a26397a93e0779963e3d22ea3b8c83ddd0df0
bahittinn <bahittin84@gmail.com>
2023-07-25 22:07:34 +0300

    New extension

--- RickyMorty/Features/RickyMorty/ViewModel/RickyMortyViewController.swift
+++ RickyMorty/Features/RickyMorty/ViewModel/RickyMortyViewController.swift
@@ -17,23 +17,43 @@ final class RickyMortyViewController: UIViewController {
         view.backgroundColor = .systemBackground
         configure()
     }
-    func configure() {
+    private func configure() {
         view.addSubview(labelTitle)
         view.addSubview(box)
         view.addSubview(indicator)
-        box.backgroundColor = .red
-        labelTitle.text = "bahittin"
-        labelTitle.snp.makeConstraints { (make) in
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
-            make.left.equalTo(view).offset(10)
-            make.right.equalTo(view).offset(-10)
-            make.height.equalTo(20)
+        drawDesign()
+        makeBox()
+        makeLabel()
+        makeIndicator()
+    }
+    
+    private func drawDesign() {
+        DispatchQueue.main.async {
+            self.box.backgroundColor = .red
+            self.labelTitle.text = "bahittin"
+            self.indicator.color = .red
+            self.indicator.startAnimating()
         }
+    }
+}
+
+extension RickyMortyViewController {
+    private func makeBox() {
         box.snp.makeConstraints { make in
             make.top.equalTo(labelTitle.snp.bottom).offset(10)
             make.bottom.equalToSuperview()
             make.left.right.equalTo(labelTitle)
         }
+    }
+    private func makeLabel() {
+        labelTitle.snp.makeConstraints { (make) in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+            make.left.equalTo(view).offset(10)
+            make.right.equalTo(view).offset(-10)
+            make.height.greaterThanOrEqualTo(20)
+        }
+    }
+    private func makeIndicator() {
         indicator.snp.makeConstraints { make in
             make.height.equalTo(labelTitle)
             make.right.equalTo(labelTitle).offset(-5)
