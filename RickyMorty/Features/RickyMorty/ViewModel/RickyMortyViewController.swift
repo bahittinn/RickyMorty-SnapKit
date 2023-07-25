6c930848e8aef1c4a8e3990e36f076f0f1a2a4ad
bahittinn <bahittin84@gmail.com>
2023-07-25 21:09:14 +0300

    Initial Commit

---
+++ RickyMorty/Features/RickyMorty/ViewModel/RickyMortyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RickyMortyViewController.swift
+//  RickyMorty
+//
+//  Created by Bahittin on 25.07.2023.
+//
+
+import UIKit
+
+final class RickyMortyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
