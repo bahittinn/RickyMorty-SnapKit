1a2d680823afe8e8e974d37b56db04996ed28112
bahittinn <bahittin84@gmail.com>
2023-07-25 21:39:35 +0300

    Home screen design

--- RickyMorty/Features/RickyMorty/ViewModel/RickyMortyViewController.swift
+++ RickyMorty/Features/RickyMorty/ViewModel/RickyMortyViewController.swift
@@ -14,7 +14,30 @@ final class RickyMortyViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .systemBackground
+        configure()
     }
-    
+    func configure() {
+        view.addSubview(labelTitle)
+        view.addSubview(box)
+        view.addSubview(indicator)
+        box.backgroundColor = .red
+        labelTitle.text = "bahittin"
+        labelTitle.snp.makeConstraints { (make) in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+            make.left.equalTo(view).offset(10)
+            make.right.equalTo(view).offset(-10)
+            make.height.equalTo(20)
+        }
+        box.snp.makeConstraints { make in
+            make.top.equalTo(labelTitle.snp.bottom).offset(10)
+            make.bottom.equalToSuperview()
+            make.left.right.equalTo(labelTitle)
+        }
+        indicator.snp.makeConstraints { make in
+            make.height.equalTo(labelTitle)
+            make.right.equalTo(labelTitle).offset(-5)
+            make.top.equalTo(labelTitle)
+        }
+    }
 }
