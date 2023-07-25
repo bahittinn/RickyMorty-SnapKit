3649b41d5f642ee04c065134ba8216e85c58d0a1
bahittinn <bahittin84@gmail.com>
2023-07-25 21:14:49 +0300

    Snapkit added

--- RickyMorty/Features/RickyMorty/ViewModel/RickyMortyViewController.swift
+++ RickyMorty/Features/RickyMorty/ViewModel/RickyMortyViewController.swift
@@ -6,24 +6,15 @@
 //
 
 import UIKit
-
+import SnapKit
 final class RickyMortyViewController: UIViewController {
-
+    private let labelTitle: UILabel = UILabel()
+    private let box:UIView = UIView()
+    private let indicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
